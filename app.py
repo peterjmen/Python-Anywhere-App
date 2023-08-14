dc363aaed23f223cf3c06d9350ddea60d20c7492
Peter menhinick <peterjmen@gmail.com>
2023-08-14 20:55:57 +0700

    change stopped working

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 # ls -R > file_structure.txt
 # ---------------------------------------------
 
+import os
 from flask import Flask, render_template, request
 import requests
 from datetime import datetime
@@ -13,7 +14,9 @@ app = Flask(__name__)
 NEWS_API_KEY = "c90bf5366948496b842fa35d8776398c"
 
 # Load the trained model
-model_filename = "sentiment_model.pkl"
+model_filename = os.path.join(
+    "/home/Manhandle/Python-Anywhere-App", "sentiment_model.pkl"
+)
 model = joblib.load(model_filename)
 print(f"Trained model loaded from '{model_filename}'")
 
